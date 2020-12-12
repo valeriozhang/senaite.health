c76d8bcf89b3090a879329272451a73c14b8a735
valeriozhang <valeriozhang14@gmail.com>
2020-12-12 18:58:12 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         "archetypes.schemaextender",
         "valeri.lims==1.3.4rc1",
-        "valeri.panic<=1.0.1",
+        "valeri.panic==1.0.1rc1",
     ],
     extras_require={
         'test': [
