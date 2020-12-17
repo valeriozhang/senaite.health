8f47bebb6e21b92f7c5db5e49dd3ce61d83ac9f4
valeriozhang <valeriozhang14@gmail.com>
2020-12-17 22:42:26 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 
 
-version = '1.2.4rc5'
+version = '1.2.4rc6'
 
 setup(
     name='valer.health',
@@ -36,8 +36,8 @@ setup(
     zip_safe=False,
     install_requires=[
         "archetypes.schemaextender",
-        "valer.lims==1.3.4rc5",
-        "valer.panic==1.0.1rc5",
+        "valer.lims==1.3.4rc6",
+        "valer.panic==1.0.1rc6",
     ],
     extras_require={
         'test': [
